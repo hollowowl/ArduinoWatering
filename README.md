1ee67c86e0a4d47e886855ab617420daa7062be9
Evgeny <ixidiciti@gmail.com>
2021-01-01 18:12:47 +0200

    Initial commit - Thuja

---
+++ README.md
@@ -0,0 +1,10 @@
+# **ArduinoWatering**  
+(**Work in progress**)  
+An attempt to make a simple automated watering system using Arduino Uno board, soil moisture sensor and immersible pump (maybe air humidity sensor for some plants).  
+
+## Plants  
++ Thuja (testing)
+
+## Current schematic  
+<img src="./schematic.svg">
+
---
+++ Thuja/Thuja.ino
@@ -0,0 +1,41 @@
+/*
+SEN0308, 5V, 2 GND
+*/
+
+int PUMP_PIN = 2;
+int SENSOR_PIN = A1;
+
+int SENSOR_AIR_VALUE = 585;
+int SENSOR_WATER_VALUE = 72;  // At 100mm - "recommended" depth
+
+float HUMIDITY_THRESHOLD = 0.5;
+int HUMIDITY_CHECK_INTERVAL_SEC = 5;
+int TURN_PUMP_FOR_SEC = 2;
+
+float calcHumidity(int sensorValue) {
+  return (float)(sensorValue - SENSOR_AIR_VALUE) / (float)(SENSOR_WATER_VALUE - SENSOR_AIR_VALUE);
+}
+
+void turnPumpFor(int ms) {
+  digitalWrite(PUMP_PIN, HIGH);
+  delay(ms);
+  digitalWrite(PUMP_PIN, LOW);
+}
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(PUMP_PIN, OUTPUT);
+}
+
+void loop() {
+  int sensorValue = analogRead(SENSOR_PIN);
+  float humidity = calcHumidity(sensorValue);
+  Serial.print("Moisture Sensor Value:");
+  Serial.println(sensorValue);
+  Serial.print("Humidity: ");
+  Serial.println(humidity);
+  if (humidity < HUMIDITY_THRESHOLD) {
+    turnPumpFor(TURN_PUMP_FOR_SEC * 1000);
+  }
+  delay(HUMIDITY_CHECK_INTERVAL_SEC * 1000);
+}
